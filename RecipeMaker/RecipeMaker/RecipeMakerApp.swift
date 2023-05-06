f539e3b60e0d702e3ab613f69ecbf9f047f0de2d
Jayme Rutkoski <jayme-rutkoski@Jayme-Rutkoski-macpro2021.local>
2023-05-06 11:59:22 -0700

    Create project

---
+++ RecipeMaker/RecipeMaker/RecipeMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeMakerApp.swift
+//  RecipeMaker
+//
+//  Created by Jayme Rutkoski on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
