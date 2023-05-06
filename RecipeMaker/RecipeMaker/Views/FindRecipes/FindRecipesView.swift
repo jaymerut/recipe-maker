427aacc6faf8a43dd30c8405785eb1d8b3572e2a
Jayme Rutkoski <jayme-rutkoski@Jayme-Rutkoski-macpro2021.local>
2023-05-06 13:13:30 -0700

    Add keywords UI for textfield and label

--- RecipeMaker/RecipeMaker/Views/FindRecipes/FindRecipesView.swift
+++ RecipeMaker/RecipeMaker/Views/FindRecipes/FindRecipesView.swift
@@ -10,11 +10,27 @@ import SwiftUI
 struct FindRecipesView: View {
 
     @ObservedObject var viewModel: ViewModel
+    @State var textValue: String = ""
     
     var body: some View {
-        VStack {
+        VStack(alignment: .center) {
             Text("Find Recipes")
+            HStack(alignment: .center) {
+                Label("Keywords", image: "")
+                    .padding(.leading, -7)
+                Spacer()
+            }
+            TextField("Enter one or more keywords", text: $textValue)
+                .textFieldStyle(.roundedBorder)
+                .padding(4)
+                .padding(.leading, 4)
+                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1)
+                         )
+            
+            Spacer()
         }
+        .padding(.vertical, 30)
+        .padding(.horizontal, 20)
     }
 }
 
