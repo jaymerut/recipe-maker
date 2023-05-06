76a3bfda1006530a362672040bdade104acb3b56
Jayme Rutkoski <jayme-rutkoski@Jayme-Rutkoski-macpro2021.local>
2023-05-06 13:23:32 -0700

    Add search button to textfield

--- RecipeMaker/RecipeMaker/Views/FindRecipes/FindRecipesView.swift
+++ RecipeMaker/RecipeMaker/Views/FindRecipes/FindRecipesView.swift
@@ -20,12 +20,22 @@ struct FindRecipesView: View {
                     .padding(.leading, -7)
                 Spacer()
             }
-            TextField("Enter one or more keywords", text: $textValue)
-                .textFieldStyle(.roundedBorder)
-                .padding(4)
-                .padding(.leading, 4)
-                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1)
-                         )
+            ZStack(alignment: .trailing) {
+                TextField("Enter one or more keywords", text: $textValue)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(4)
+                    .padding(.leading, 4)
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1)
+                             )
+                Image("search")
+                    .renderingMode(.template)
+                    .resizable()
+                    .padding(5)
+                    .background(.green)
+                    .foregroundColor(.white)
+                    .frame(width: 39.0, height: 40.0)
+                    .cornerRadius(8, corners: [.topRight, .bottomRight])
+            }
             
             Spacer()
         }
@@ -37,5 +47,22 @@ struct FindRecipesView: View {
 struct FindRecipesView_Previews: PreviewProvider {
     static var previews: some View {
         FindRecipesView(viewModel: .init())
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
     }
 }
